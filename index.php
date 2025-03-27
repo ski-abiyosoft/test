ff618a969093248053ae8747a564931054f86121
M. Donny Irwansyah <irwansyah.jr27@gmail.com>
2025-03-28 02:49:40 +0700

    Update index.php in feature

--- index.php
+++ index.php
@@ -9,8 +9,4 @@ echo "Test";
 echo "Test";
 echo "Test";
 echo "Test";
-<<<<<<< main
 echo "Selamat datang di branch MAIN!";
-=======
-echo "Selamat datang di branch FEATURE!";
->>>>>>> feature
---
+++ modules/test.php
@@ -0,0 +1,6 @@
+<?php
+<<<<<<< HEAD
+echo "Selamat datang di branch MAIN!";
+=======
+echo "Selamat datang di branch FEATURE!";
+>>>>>>> feature
